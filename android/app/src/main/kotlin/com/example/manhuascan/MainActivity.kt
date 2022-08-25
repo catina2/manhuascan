fe363bdea27d6998e1bea4b16eabe6e05724a068
catina2 <caterina2@tkhplanesw.comm>
2022-08-25 14:58:43 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/manhuascan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.manhuascan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_applovin_max/flutter_applovin_max.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class AppLovinAds extends StatefulWidget {
+  const AppLovinAds({Key? key}) : super(key: key);
+
+  @override
+  State<AppLovinAds> createState() => _AppLovinAdsState();
+}
+
+class _AppLovinAdsState extends State<AppLovinAds> {
+  bool isInterstitialVideoAvailable = false;
+  bool isRewardedVideoAvailable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterApplovinMax.initRewardAd('d8ec28af779f894f');
+    FlutterApplovinMax.initInterstitialAd('eb71ced9891aaf40');
+  }
+
+  void listener(AppLovinAdListener event) {
+    print(event);
+    if (event == AppLovinAdListener.onUserRewarded) {
+      print('👍get reward');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  isInterstitialVideoAvailable =
+                      (await FlutterApplovinMax.isInterstitialLoaded(
+                          (listener) => null))!;
+
+                  if (isInterstitialVideoAvailable) {
+                    FlutterApplovinMax.showInterstitialVideo(
+                        (AppLovinAdListener? event) => listener(event!));
+                  }
+                },
+                child: const Text('')),
+            ElevatedButton(
+                onPressed: () async {
+                  isRewardedVideoAvailable =
+                      (await FlutterApplovinMax.isRewardLoaded(
+                          (listener) => null))!;
+
+                  if (isRewardedVideoAvailable) {
+                    FlutterApplovinMax.showRewardVideo(
+                        (AppLovinAdListener? event) => listener(event!));
+                  }
+                },
+                child: const Text(''))
+          ],
+        ),
+      ),
+    );
+  }
+}
