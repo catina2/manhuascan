3d712238a0f67467bacdab1d7de061e0ca1df7df
catina2 <caterina2@tkhplanesw.comm>
2022-08-26 16:29:38 +0700

    six commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_applovin_max/flutter_applovin_max.dart';
 import 'home_page.dart';
 
 void main() {
@@ -18,71 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class AppLovinAds extends StatefulWidget {
-  const AppLovinAds({Key? key}) : super(key: key);
-
-  @override
-  State<AppLovinAds> createState() => _AppLovinAdsState();
-}
-
-class _AppLovinAdsState extends State<AppLovinAds> {
-  bool isInterstitialVideoAvailable = false;
-  bool isRewardedVideoAvailable = false;
-
-  @override
-  void initState() {
-    super.initState();
-    FlutterApplovinMax.initRewardAd('d8ec28af779f894f');
-    FlutterApplovinMax.initInterstitialAd('eb71ced9891aaf40');
-  }
-
-  void listener(AppLovinAdListener event) {
-    print(event);
-    if (event == AppLovinAdListener.onUserRewarded) {
-      print('👍get reward');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () async {
-                  isInterstitialVideoAvailable =
-                      (await FlutterApplovinMax.isInterstitialLoaded(
-                          (listener) => null))!;
-
-                  if (isInterstitialVideoAvailable) {
-                    FlutterApplovinMax.showInterstitialVideo(
-                        (AppLovinAdListener? event) => listener(event!));
-                  }
-                },
-                child: const Text('')),
-            ElevatedButton(
-                onPressed: () async {
-                  isRewardedVideoAvailable =
-                      (await FlutterApplovinMax.isRewardLoaded(
-                          (listener) => null))!;
-
-                  if (isRewardedVideoAvailable) {
-                    FlutterApplovinMax.showRewardVideo(
-                        (AppLovinAdListener? event) => listener(event!));
-                  }
-                },
-                child: const Text(''))
-          ],
-        ),
-      ),
     );
   }
 }
